16b963711e7e496b53933a177e764f04a30ca257
Vincent Ekpanyaskun <vincent.ekp@gmail.com>
2024-06-09 18:24:35 +1000

    Initial commit

---
+++ bilingual_bee/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bilingual_bee project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bilingual_bee.settings')
+
+application = get_wsgi_application()
