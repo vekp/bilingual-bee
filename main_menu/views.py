ad283fd3520c1a80d4361b684474b40714aa88b6
Vincent Ekpanyaskun <vincent.ekp@gmail.com>
2024-06-10 15:18:58 +1000

    Start work on main menu

--- main_menu/views.py
+++ main_menu/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse #TODO remove
 
 # Create your views here.
+def index(request):
+    return render(request, 'base.html')
+    return HttpResponse('Hello!! Welcome.')#TODO remove
