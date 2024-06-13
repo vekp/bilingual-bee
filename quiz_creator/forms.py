91033aafd1f7fda1273451a73ad79650a504f118
Vincent Ekpanyaskun <vincent.ekp@gmail.com>
2024-06-14 06:18:49 +1000

    Add question selection to quiz detail view

--- quiz_creator/forms.py
+++ quiz_creator/forms.py
@@ -1,8 +1,23 @@
 from django import forms
 
-from .models import Quiz
+from .models import Quiz, Question
 
-class QuizForm(forms.ModelForm):
+class NewQuizForm(forms.ModelForm):
     class Meta:
         model = Quiz
-        fields = ['quiz_name', 'questions']
+        fields = ['quiz_name']
+        
+class QuizDetailForm(forms.ModelForm):
+    questions = forms.ModelMultipleChoiceField(
+        queryset=Question.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+    class Meta:
+        model = Quiz
+        fields = ['questions']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['questions'].initial = self.instance.questions.all()        
+        
--- quiz_creator/views.py
+++ quiz_creator/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import *
-from .forms import QuizForm
+from .forms import *
 
 # Create your views here.
 
 
 def quiz_list(request):
+    # Display list of all quizzes
     if request.method == "GET":
         quizzes = Quiz.objects.all()
-        form = QuizForm()
+        form = NewQuizForm()
         return render(
             request,
             'quiz_creator/quiz_list.html',
@@ -17,11 +18,32 @@ def quiz_list(request):
         )
     # New quiz, POST request
     else:
-        form = QuizForm(request.POST)
+        form = NewQuizForm(request.POST)
+        # Create new quiz, redirect to its edit page
         if form.is_valid:
             new_quiz = form.save()
+            return redirect('quiz_detail', new_quiz.pk)
+        else:
+            return HttpResponse("Invalid input")
+            
 
 
 def quiz_detail(request, pk):
+    all_questions = Question.objects.all()
     quiz = get_object_or_404(Quiz, pk=pk)
-    return (HttpResponse(quiz))
+    form = QuizDetailForm()
+    if request.method == "GET":
+        return (render(
+            request,
+            'quiz_creator/quiz_detail.html',
+            {'quiz': quiz, 'form': form, 'all_questions': all_questions}
+        ))
+    else:
+        selected_questions = request.POST.getlist("question")
+        for question in all_questions:
+            if str(question.pk) in selected_questions:
+                quiz.questions.add(question)
+            else:
+                quiz.questions.remove(question)
+        return HttpResponse("Nice")
+        
